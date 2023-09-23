24e4b56b5de6aa4d753c7c0cede330a16d08e332
Tam <hi@tam.sx>
2023-09-23 12:32:36 +0100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+pub const TILE_SIZE : f32 = 16.;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::hex("#6E3E38").unwrap()))
@@ -18,6 +20,51 @@ fn main() {
 
 fn setup (
     mut commands : Commands,
+    asset_server : Res<AssetServer>,
+    mut texture_atlases : ResMut<Assets<TextureAtlas>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    // Camera
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scale: 0.5,
+            ..default()
+        },
+        ..default()
+    });
+    
+    // Texture
+    let texture_atlas = TextureAtlas::from_grid(
+        asset_server.load("tilemap.png"),
+        Vec2::splat(16.),
+        12, 11,
+        Some(Vec2::splat(1.)), None
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    
+    for x in -18..=18 {
+        for y in -10..=10 {
+            let i = if x == -18 {
+                if y == 10 { 4 }
+                else if y == -10 { 16 }
+                else { 15 }
+            } else if x == 18 {
+                if y == 10 { 5 }
+                else if y == -10 { 17 }
+                else { 13 }
+            } else {
+                if y == 10 { 26 }
+                else if y == -10 { 2 }
+                else { 0 }
+            };
+            
+            commands.spawn((
+                SpriteSheetBundle {
+                    texture_atlas: texture_atlas_handle.clone(),
+                    sprite: TextureAtlasSprite::new(i),
+                    transform: Transform::from_xyz(TILE_SIZE * x as f32, TILE_SIZE * y as f32, 0.),
+                    ..default()
+                },
+            ));
+        }
+    }
 }
